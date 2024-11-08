0e0e513b89b149851329040da6d2547981dbbd13
Sandeep prajapati <61901052+sandeep-sparrow@users.noreply.github.com>
2024-11-08 11:52:28 +0530

    added selection sort logic

--- src/main/java/com/engineeringwithsandeep/learning/youtube/algorithms/sort/Sorts.java
+++ src/main/java/com/engineeringwithsandeep/learning/youtube/algorithms/sort/Sorts.java
@@ -28,6 +28,22 @@ public class Sorts {
         }
     }
 
+    public static void selectionSort(int[] myArray) {
+        for(int i = 0; i < myArray.length - 1; i++) {
+            int minIndex = i;
+            for(int j = i + 1; j < myArray.length; j++) {
+                if (myArray[j] < myArray[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = myArray[i];
+                myArray[i] = myArray[minIndex];
+                myArray[minIndex] = temp;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] myArray = {4,2,6,5,1,3};
         bubbleSort(myArray);
